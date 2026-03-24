8d2874870472593a903ee0a9a1d7dc084030bf4c
McCarthyDe <mcde8683@colorado.edu>
2026-03-24 08:38:10 -0600

    Implement linearized equations of motion for quadrotor

---
+++ linerarizedEOM.m
@@ -0,0 +1,31 @@
+function var_dot = QuadrotorEOM_linearized(t, var, g, m, I, deltaFc, deltaGc)
+
+var_dot = zeros(12, 1);
+
+x_E = var(1);
+y_E = var(2);
+z_E = var(3);
+phi = var(4);
+theta = var(5);
+psi = var(6);
+u_E = var(7);
+v_E = var(8);
+w_E = var(9);
+p = var(10);
+q = var(11);
+r = var(12);
+
+var_dot(1) = u_E;
+var_dot(2) = v_E;
+var_dot(3) = w_E;
+var_dot(4) = p;
+var_dot(5) = q;
+var_dot(6) = r;
+var_dot(7) = -g*theta;
+var_dot(8) =  g*phi; 
+var_dot(9) = deltaFc/m; 
+var_dot(10) = deltaGc(1)/ I(1,1); 
+var_dot(11) = deltaGc(2)/ I(2,2); 
+var_dot(12) = deltaGc(3)/ I(3,3);
+
+end
